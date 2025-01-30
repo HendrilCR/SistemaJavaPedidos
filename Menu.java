c3a83c167a48406fb42434a1da9d02b093db5b61
HendrilCR <hendrilcr2004@gmail.com>
2025-01-30 15:53:20 -0200

    2

--- Menu.java
+++ Menu.java
@@ -7,15 +7,13 @@ public class Menu {
     Menu(){
         s1 = new Scanner(System.in);
 
-        System.out.print("\nDigite uma opção:\n\t[Default] Testar Sistema\n\t[1] Criar Produto\n--> ");
+        System.out.print("\nDigite uma opção:\n\t[Default] Testar Sistema\n--> ");
 
 
         op = s1.nextLine();
         System.out.println(op);
 
         switch(op){
-            case "1":
-                break;
             default:
                 Main.testar();
                 break;
